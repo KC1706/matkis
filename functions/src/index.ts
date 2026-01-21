7cd11664dce994d6d6044c58003b7ecc72362954
Kc23mj <kunalchandoa925@gmail.com>
2026-01-21 23:11:01 +0530

    completed

--- functions/src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as admin from 'firebase-admin';
-
-admin.initializeApp();
-
-// Export functions
-export { getLeaderboard } from './leaderboard';
-export { searchUsers } from './search';
-export { seedUsers } from './seed';
